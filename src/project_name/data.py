919541b2fb5ae1bc57a31b9cdb6887a9ae6dd6a5
YuxinLiu42 <liuyuxinnj@gmail.com>
2026-05-11 15:51:02 +0200

    fix mypy errors

--- src/project_name/data.py
+++ src/project_name/data.py
@@ -12,9 +12,11 @@ class MyDataset(Dataset):
 
     def __len__(self) -> int:
         """Return the length of the dataset."""
+        return 0
 
     def __getitem__(self, index: int):
         """Return a given sample from the dataset."""
+        return None
 
     def preprocess(self, output_folder: Path) -> None:
         """Preprocess the raw data and save it to the output folder."""
